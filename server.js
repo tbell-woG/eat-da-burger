3e9a4f2d8350b2207d6b3be24ece3e94f427bc3c
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-12 01:21:19 -0500

    Completed some of server.js and main.hbs

---
+++ server.js
@@ -0,0 +1,30 @@
+// These require the dependencies
+var express = erquire("express");
+var bodyParser = require("body-parser")
+
+// These are the variables for the server and express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// This serves the static content from the public directory
+app.use(express.static("public"));
+
+//This parses application/urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//This parses application/json
+app.use(bodyParser.json());
+
+// This sets up the Express Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// This imports the routes and allows the server to access them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});

bfffbec79740f732a69a038192adb4c8265b25d9
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-12 02:53:47 -0500

    Added on click funtions for buttons in burger.js

---
+++ public/assets/js/burger.js
@@ -0,0 +1,57 @@
+// This is the function that has both of the on click functions & waits for the DOM to be loaded
+$(function () {
+
+    // This adds a new burger
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newburger").val().trim(),
+            devoured: 0
+        };
+
+        // This sends the POST request
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        })
+        .then(function() {
+            console.log("Added a new burger");
+            // This reloads the page to display the updated burger list
+            location.reload();
+        });
+    });
+
+    $(".eatburger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+
+        // This sends the PUT request
+        $ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        })
+        .then(function() {
+            console.log("The burger is devoured");
+            location.reload();
+        });
+    });
+
+    $(".trashburger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        // This sends the DELETE request
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        })
+        .then(location.reload());
+    });
+
+})
